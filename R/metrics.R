c03aa5475771646546a7435726d3ba24fb6d4f86
JohnCoene <jcoenep@gmail.com>
2020-12-05 14:04:52 +0100

    metrics + local storage figured

--- R/metrics.R
+++ R/metrics.R
@@ -1,8 +1,162 @@
+#' Metric
+#' 
+#' Metric system build on top of the storage.
+#' 
+#' @author John Coene, \email{john@@opifex.org}
+#' 
+#' @noRd 
+#' @keywords internal
 Metric <- R6::R6Class(
   "Metric",
   inherit = Store,
+  public = list(
+    type = function(type = c("counter", "gauge", "histogram", "summary")){
+      private$.type <- match.arg(type)
+      invisible(self)
+    },
+    name = function(text){
+      stopMissing(text, "name")
+      private$checkName(text)
+      private$.name <- text
+      invisible(self)
+    },
+    help = function(text){
+      stopMissing(text, "help")
+      private$.help <- text
+      invisible(self)
+    },
+    print = function(){
+      cat("A", private$.type, "\n")
+    }
+  ),
   private = list(
+    .name = "",
+    .help = "",
     .type = "counter",
-    .store = NULL
+    checkName = function(name){
+      hasSpace <- grep("\\s", name)
+      doubleUnderscore <- grepl("^__", name)
+      if(any(hasSpace, doubleUnderscore))
+        stop(
+          "Incorrect name, may not start with `__` or include spaces", 
+          call. = FALSE
+        )
+    }
   )
 )
+
+#' Counter
+#' 
+#' Counter for Prometheus. The value of a counter,
+#' can only increase over time.
+#' 
+#' @author John Coene, \email{john@@opifex.org}
+#' 
+#' @export
+Counter <- R6::R6Class(
+  "Counter",
+  public = list(
+#' @details Initialise a counter
+#' @param name The name of the counter.
+#' @param help The help text discribing the counter.
+#' @param labels Labels to use for this counter, a
+#' character vector.
+#' 
+#' @examples 
+#' c <- Counter$new(
+#'  "refresh_btn_total", 
+#'  "Button that refreshes the data",
+#'  labels = c("module")
+#' ) 
+#' 
+#' c$inc(1, module = "homepage")
+#' c$inc(1, module = "tableview")
+    initialize = function(name, help, labels = NULL){
+      private$.metric <- Metric$
+        new(labels)$
+        type("counter")$
+        name(name)$
+        help(help)
+    },
+#' @details Increase the value of the counter.
+#' 
+#' @param val Value to increase the counter by.
+#' @param ... Named label and value pair(s). 
+#' 
+#' @examples 
+#' c <- Counter$new(
+#'  "refresh_btn_total", 
+#'  "Loads data button count",
+#'  labels = c("module")
+#' ) 
+#' 
+#' c$inc(1, module = "homepage")
+#' c$inc(1, module = "tableview")
+    inc = function(val = 1, ...){
+      private$.value <- private$.value + val
+      private$.metric$setValue(private$.value, ...)
+      invisible(self)
+    },
+#' @details Set the counter to a certain value.
+#' 
+#' @param val Value to set the counter to.
+#' @param ... Named label and value pair(s). 
+#' 
+#' @examples 
+#' c <- Counter$new(
+#'  "refresh_btn_total", 
+#'  "Loads data button count"
+#' ) 
+#' 
+#' c$set(1)
+#' c$set(5)
+#' 
+#' # will throw warning: 3 < 5
+#' \dontrun{c$set(3)}
+    set = function(val, ...){
+      if(missing(val)){
+        warnMissing(val)
+        return(invisible())
+      }
+
+      if(val < private$.value){
+        warning(
+          "`val` too low, must be higher than: ", 
+          private$.value, " [ignoring]", 
+          call. = FALSE
+        )
+        return(invisible())
+      }
+      private$.value <- val
+      private$.metric$setValue(private$.value, ...)
+      invisible(self)
+    },
+#' @details Print the counter.
+    print = function(){
+      print(private$.metric)
+    }
+  ),
+  private = list(
+    .value = 0,
+    .metric = NULL
+  )
+)
+
+#' Warn an argument is missing
+#' 
+#' Warn that an argument is missing.
+#' 
+#' @param what Name of argument (string) that is missing.
+#' 
+#' @noRd 
+#' @keywords internal
+warnMissing <- function(what){
+  warning("Missing `", what, "` [ignoring]", call. = FALSE)
+}
+
+#' @noRd 
+#' @keywords internal
+stopMissing <- function(var, what){
+  if(missing(var))
+    stop("Missing `", what, "`", call. = FALSE)
+}
--- R/store.R
+++ R/store.R
@@ -1,25 +1,157 @@
+#' Storage
+#' 
+#' Simple storage system for a single metric.
+#' 
+#' @author John Coene, \email{john@@opifex.org}
+#' 
+#' @importFrom dplyr anti_join bind_rows
+#' 
+#' @noRd 
+#' @keywords internal
 Store <- R6::R6Class(
   "Store",
   public = list(
-    initialize = function(name, type = c("counter", "gauge", "histogram", "summary"), labels = ""){
-      df <- data.frame(labels = labels, values = 0)
-      private$.data <- df
-      private$.name <- name
+    initialize = function(labels = NULL){
+      private$.labels <- labels
+      private$.data <- initStorage(labels)
     },
-    getName = function(){
-      private$.name
+    setValue = function(val, ...){
+      labels <- labelsOrNull(...)
+      match <- private$checkLabelsMatch(labels)
+      
+      if(!match) return(invisible())
+
+      private$upsert(val, labels)
+
+      invisible(self)
+    },
+    getData = function(){
+      return(private$.data)
     }
   ),
   private = list(
-    .name = "",
-    .type = "counter",
+    .labels = NULL,
     .data = data.frame(),
-    setVal = function(val, label = ""){
-      private$.data[private$.data$labels == label, "values"] <- val
-      invisible(self)
+    upsert = function(val, labels = NULL){
+      newLabels <- list(value = val, labels)
+      newLabels <- dropNulls(newLabels)
+      newData <- as.data.frame(newLabels)
+
+      if(is.null(labels)){
+        private$.data <- newData
+        return()
+      }
+
+      old <- anti_join(private$.data, newData, by = private$.labels)
+      data <- tryCatch(
+        bind_rows(old, newData),
+        error = function(e) e
+      )
+
+      if(inherits(data, "error")){
+        warning(
+          "Failed to process metric value - ", 
+          data, 
+          " [ignoring]",
+          call. = FALSE
+        )
+        return()
+      }
+
+      private$.data <- data
     },
-    getVal = function(label = ""){
-      private$.data[private$.data$labels == label, "values"]
+    checkLabelsMatch = function(labels = NULL){
+      m <- all(private$.labels %in% names(labels))
+
+      if(!m) private$wrongLabels(labels)
+      
+      return(m)
+    },
+    wrongLabels = function(labels = NULL){
+      existing <- labelToString(private$.labels)
+      passed <- labelToString(labels)
+      warning(
+        "Labels mismatch, must have ", existing, " received ", passed,
+        call. = FALSE
+      )
     }
   )
 )
+
+#' Labels to string
+#' 
+#' Turns labels to single strings for use in warning messages.
+#' 
+#' @param labels Vector of labels.
+#' 
+#' @return A character vector of length one. If `lables` is 
+#' `NULL` returns `"none"`.
+#' 
+#' @author John Coene, \email{john@@opifex.org}
+#' 
+#' @noRd 
+#' @keywords internal
+labelToString <- function(labels = NULL){
+  if(is.null(labels))
+    return("none")
+  lab <- paste(labels, collapse = "`, `")
+  paste0("`", lab, "`")
+}
+
+#' Labels of NULL
+#' 
+#' Returns vector of character labels or `NULL`
+#' if nothing is passed to `...`.
+#' 
+#' @param ... Labels to register.
+#' 
+#' @return Vector of labels or `NULL`.
+#' 
+#' @author John Coene, \email{john@@opifex.org}
+#' 
+#' @noRd 
+#' @keywords internal
+labelsOrNull <- function(...){
+  labs <- list(...)
+  if(length(labs) == 0)
+    return(NULL)
+
+  return(labs)
+}
+
+#' Initialise the Storage
+#' 
+#' Initialse the storage based on the labels used.
+#' 
+#' @param labels A vector of labels or `NULL` if none
+#' are used. 
+#' 
+#' @return A `data.frame`.
+#' 
+#' @noRd 
+#' @keywords internal
+initStorage <- function(labels = NULL){
+
+  df <- data.frame(value = numeric())
+  for(lab in labels){
+    df[[lab]] <- character()
+  }
+
+  return(df)
+}
+
+#' Drop nulls
+#' 
+#' Removes `NULL` entries from `list`.
+#' 
+#' @param x A `list`.
+#' 
+#' @return A modified version of the input `list`.
+#' 
+#' @author John Coene, \email{john@@opifex.org}
+#' 
+#' @noRd 
+#' @keywords internal
+dropNulls <- function(x) {
+  x[!vapply(x, is.null, FUN.VALUE = logical(1))]
+}
