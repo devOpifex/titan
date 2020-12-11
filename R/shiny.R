7a62b713507bee7a650e573740ddaf0ae4ac38e4
John (JP) Coene <jcoenep@gmail.com>
2020-12-11 09:51:40 +0100

    Update shiny.R

--- R/shiny.R
+++ R/shiny.R
@@ -136,7 +136,7 @@ titanApp <- function(ui, server, ..., inputs = NULL, visits = NULL,
       onSessionEnd <- onEnd(concurrentGauge, durationHist)
       shiny::onSessionEnded(onSessionEnd)
 
-      serverFnSource()
+      serverFnSource(input, output, session)
     }
   }
 
